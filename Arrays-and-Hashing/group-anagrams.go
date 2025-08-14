9abdf22274a8ff089ae120cb74481508e7fa1514
Dzkzzz <dzdzaakk@gmail.com>
2025-08-14 22:38:16 +0700

    group anagrams

--- Arrays-and-Hashing/group-anagrams.go
+++ Arrays-and-Hashing/group-anagrams.go
@@ -1,42 +1,58 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
-
+// O(m*n)
 func groupAnagrams(words []string) [][]string {
-	anagramGroups := make(map[string][]string)
+	mapGroup := make(map[[26]int][]string)
 
-	for _, word := range words {
-		key := generateRuneKey(word)
+	for _, s := range words {
+		var count [26]int
+		for _, c := range s {
+			count[c-'a']++
+		}
+		mapGroup[count] = append(mapGroup[count], s)
 
-		anagramGroups[key] = append(anagramGroups[key], word)
 	}
 
-	var result [][]string
-	for _, group := range anagramGroups {
+	result := make([][]string, 0, len(mapGroup))
+	for _, group := range mapGroup {
 		result = append(result, group)
 	}
 
 	return result
 }
 
-func generateRuneKey(word string) string {
-	runeCount := make([]int, 26)
+// func groupAnagrams(words []string) [][]string {
+// 	anagramGroups := make(map[string][]string)
 
-	for _, r := range word {
-		runeCount[r-'a']++
-	}
+// 	for _, word := range words {
+// 		key := generateRuneKey(word)
 
-	var keyBuilder strings.Builder
-	for _, count := range runeCount {
-		keyBuilder.WriteString(strconv.Itoa(count))
-		keyBuilder.WriteRune('#')
-	}
+// 		anagramGroups[key] = append(anagramGroups[key], word)
+// 	}
 
-	return keyBuilder.String()
-}
+// 	var result [][]string
+// 	for _, group := range anagramGroups {
+// 		result = append(result, group)
+// 	}
+
+// 	return result
+// }
+
+// func generateRuneKey(word string) string {
+// 	runeCount := make([]int, 26)
+
+// 	for _, r := range word {
+// 		runeCount[r-'a']++
+// 	}
+
+// 	var keyBuilder strings.Builder
+// 	for _, count := range runeCount {
+// 		keyBuilder.WriteString(strconv.Itoa(count))
+// 		keyBuilder.WriteRune('#')
+// 	}
+
+// 	return keyBuilder.String()
+// }
 
 // func main() {
 // 	strs := []string{"act", "pots", "tops", "cat", "stop", "hat"}
