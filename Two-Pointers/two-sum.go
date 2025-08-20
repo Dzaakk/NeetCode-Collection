650954062ae30954ff9feeea0b51bcffa8284d13
Dzkzzz <dzdzaakk@gmail.com>
2025-08-20 20:49:54 +0700

    two sum input array is sorted

--- Two-Pointers/two-sum.go
+++ Two-Pointers/two-sum.go
@@ -4,12 +4,13 @@ func twoSum(numbers []int, target int) []int {
 	l, r := 0, len(numbers)-1
 
 	for l < r {
-		curSum := numbers[l] + numbers[r]
-		if curSum > target {
+		if numbers[l]+numbers[r] > target {
 			r--
-		} else if curSum < target {
+		}
+		if numbers[l]+numbers[r] < target {
 			l++
-		} else {
+		}
+		if numbers[l]+numbers[r] == target {
 			return []int{l + 1, r + 1}
 		}
 	}
