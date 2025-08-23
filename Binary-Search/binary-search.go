80a75997e2902d77b474616f4cdd7b8ac82b218a
Dzkzzz <dzdzaakk@gmail.com>
2025-08-23 23:28:23 +0700

    binary search

--- Binary-Search/binary-search.go
+++ Binary-Search/binary-search.go
@@ -1,17 +1,17 @@
 package main
 
 func search(nums []int, target int) int {
-	l, r := 0, len(nums)-1
+	left, right := 0, len(nums)-1
 
-	for l <= r {
-		m := l + (r-l)/2
+	for left <= right {
+		mid := left + (right-left)/2
 
-		if nums[m] > target {
-			r = m - 1
-		} else if nums[m] < target {
-			l = m + 1
-		} else {
-			return m
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
 		}
 	}
 	return -1
