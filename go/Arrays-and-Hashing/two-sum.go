801db1e319a429e78419f7798c00cd99ab36b2b3
Dzkzzz <dzdzaakk@gmail.com>
2025-09-30 22:27:30 +0700

    two sum

--- go/Arrays-and-Hashing/two-sum.go
+++ go/Arrays-and-Hashing/two-sum.go
@@ -1,48 +1,17 @@
 package main
 
-// O(n2)
-// func twoSum(nums []int, target int) [2]int {
-// 	idx := [2]int{}
-// 	for i := 0; i < len(nums); i++ {
-// 		a := nums[i]
-// 		for j := 0; j < len(nums); j++ {
-// 			if i == j {
-// 				continue
-// 			}
-// 			b := nums[j]
-
-// 			if a+b == target {
-// 				idx[0] = i
-// 				idx[1] = j
-
-// 				return idx
-// 			}
-// 		}
-
-// 	}
-// 	return idx
-// }
-
-// better approach O(n)
+//11.40
 func twoSum(nums []int, target int) []int {
-	mapNum := make(map[int]int)
+	m := make(map[int]int)
 
-	for i, n := range nums {
-		diff := target - n
-		if j, found := mapNum[diff]; found && j != i {
-			return []int{i, j}
+	for i, v := range nums {
+		dif := target - v
+
+		if j, ok := m[dif]; ok {
+			return []int{j, i}
 		}
-		mapNum[n] = i
+		m[v] = i
 	}
-	return []int{}
+
+	return []int{0, 0}
 }
-
-// func main() {
-// 	nums1 := []int{3, 4, 5, 6}
-// 	target1 := 7
-// 	nums2 := []int{4, 5, 6}
-// 	target2 := 10
-
-// 	fmt.Println(twoSum(nums1, target1))
-// 	fmt.Println(twoSum(nums2, target2))
-// }
